f1cf061b58ed91997c13347b3d8d4cba1d901737
Alex-Hardin <johaleia@protonmail.com>
2024-10-03 15:45:15 -0500

    player made and centered in screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
+from player import Player
+from circleshape import CircleShape
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -13,15 +15,19 @@ def main():
     # Sets up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Create the player object
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                Return
+                return
         # fill screen display with black https://www.pygame.org/docs/ref/surface.html#pygame.Surface.fill
         screen.fill((0,0,0))
+        player.draw(screen)
         # update the display  https://www.pygame.org/docs/ref/display.html#pygame.display.flip
         pygame.display.flip()
-    
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
